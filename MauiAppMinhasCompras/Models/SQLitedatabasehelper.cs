a9464be6b2b9199b5426963531b1e0c8303d3ed9
Usuario <Usuario@CPD03-M15>
2024-11-04 15:10:30 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Models/SQLitedatabasehelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class SQLitedatabasehelper
+    {
+        readonly SQLitedatabasehelper _conn;
+
+        public SQLitedatabasehelper(string path)
+        {
+            _conn = new SQLitedatabasehelper(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+        public Task <int> Insert(Produto p)
+        {
+
+        }
+    }
+}
